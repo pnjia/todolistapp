aa7147ab369673c8bd499d0a3a40794409878fa9
pnjia <panjiangkasaputra67@gmail.com>
2025-11-03 07:15:26 +0700

    Menambahkan validator untuk inputtan dari user, pada login ataupun register

--- src/modules/auth/auth.routes.js
+++ src/modules/auth/auth.routes.js
@@ -2,10 +2,12 @@ import express from "express";
 import { login, register, logout } from "./auth.controllers.js";
 import { authMiddleware } from "../../middlewares/authMiddleware.js";
 import { refreshToken } from "./auth.controllers.js";
+import { loginValidation, registerValidation } from "./auth.validation.js";
+import { validatorMiddleware } from "../../middlewares/validator.js";
 const router = express.Router();
 
-router.post("/register", register);
-router.post("/login", login);
+router.post("/register", registerValidation, validatorMiddleware, register);
+router.post("/login", loginValidation, validatorMiddleware, login);
 router.post("/logout", authMiddleware, logout);
 router.post("/refresh", refreshToken);
 router.get("/", authMiddleware, (req, res) => {
