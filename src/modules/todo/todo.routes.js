47da1e5d391f9843fcddff48410609cb43fd8c52
pnjia <panjiangkasaputra67@gmail.com>
2025-11-06 08:49:17 +0700

    Menambahkan rute untuk CRUD

--- src/modules/todo/todo.routes.js
+++ src/modules/todo/todo.routes.js
@@ -1,16 +1,24 @@
 import express from "express";
+import { authMiddleware } from "../../middlewares/authMiddleware.js";
+import {
+  completeTodo,
+  createTodo,
+  deleteTodo,
+  editTodo,
+  getTodoById,
+  getTodos,
+  restoreTodo,
+} from "./todo.controllers.js";
+import { userMiddleware } from "../../middlewares/userMiddleware.js";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("Todos get all todos");
-});
-// router.post("/");
-// router.get("/");
-// router.get("/:id");
-// router.put("/:id");
-// router.patch("/:id/complete");
-// router.delete("/:id");
-// router.post("/:id/restore");
+router.get("/", authMiddleware, userMiddleware, getTodos);
+router.post("/", authMiddleware, userMiddleware, createTodo);
+router.get("/:id", authMiddleware, userMiddleware, getTodoById);
+router.put("/:id", authMiddleware, userMiddleware, editTodo);
+router.patch("/:id/complete", authMiddleware, userMiddleware, completeTodo);
+router.delete("/:id", authMiddleware, userMiddleware, deleteTodo);
+router.post("/:id/restore", authMiddleware, userMiddleware, restoreTodo);
 
 export default router;
